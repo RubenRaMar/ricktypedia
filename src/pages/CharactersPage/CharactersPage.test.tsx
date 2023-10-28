384f246e0a604bf8a3f9dae6cd27af4314aa5420
RubenRaMar <rubenfr1@hotmail.com>
2023-10-29 00:29:06 +0200

    Test CharacterPage page and their uses case

---
+++ src/pages/CharactersPage/CharactersPage.test.tsx
@@ -0,0 +1,21 @@
+import { screen, waitFor } from "@testing-library/react";
+import renderWithProviders from "../../testUtils/renderWithProviders";
+import CharactersPage from "./CharactersPage";
+
+describe("Given a CharactersPage page", () => {
+  describe("When its rendered", () => {
+    test("Then it should show a heading with de text 'Characters'", async () => {
+      const expectedHeadingText = /characters/i;
+
+      renderWithProviders({ ui: <CharactersPage /> });
+
+      await waitFor(() => {
+        const heading = screen.getByRole("heading", {
+          name: expectedHeadingText,
+        });
+
+        expect(heading).toBeInTheDocument();
+      });
+    });
+  });
+});
