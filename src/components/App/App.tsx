3272fc2b74cbf49268ccc5a452b0a2bb58a699c6
RubenRaMar <rubenfr1@hotmail.com>
2023-10-28 07:57:29 +0200

    Create ContainerStyled component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,11 @@
-const App = (): JSX.Element => {
-  return <h1>Rick y Morty</h1>;
+import ContainerStyled from "../../styles/shared/ContainerStyled";
+
+const App = (): React.ReactElement => {
+  return (
+    <ContainerStyled>
+      <h1>Rick y Morty</h1>
+    </ContainerStyled>
+  );
 };
 
 export default App;
