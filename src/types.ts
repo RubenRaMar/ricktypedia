d927ec4f07e43207d663f0168c8efc785492c097
RubenRaMar <rubenfr1@hotmail.com>
2023-10-29 02:44:37 +0200

    Create loadCharacters reduder

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export interface NameStructure {
+  name: string;
+}
+
+export interface CharacterStructure {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: NameStructure;
+  location: NameStructure;
+  image: string;
+  episode: string[];
+  created: string;
+}
+
+export interface InfoStructure {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+}
+
+export interface CharacterStateStructure {
+  results: CharacterStructure[];
+  info: InfoStructure;
+}
