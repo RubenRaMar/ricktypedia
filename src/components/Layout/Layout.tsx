f46c617bfa8662d57ad43e2bb78db12ffb3701aa
RubenRaMar <rubenfr1@hotmail.com>
2023-10-29 15:27:56 +0100

    Implement getCharacterList function in CaractersPage

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import MainHeader from "../MainHeader/MainHeader";
 import ContainerStyled from "../../styles/shared/ContainerStyled";
+import Loading from "../Loading/Loading";
 
 const Layout = (): React.ReactElement => {
   return (
     <>
+      <Loading />
       <MainHeader />
       <ContainerStyled>
         <Outlet />
