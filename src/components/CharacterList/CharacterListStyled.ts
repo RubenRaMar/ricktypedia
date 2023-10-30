34e36695d970df3db332e22f894cff5082aa4059
RubenRaMar <rubenfr1@hotmail.com>
2023-10-30 20:02:31 +0100

    Implement styles to the CharacterList component

---
+++ src/components/CharacterList/CharacterListStyled.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const CharacterListStyled = styled.ul`
+  display: grid;
+  gap: 50px;
+
+  @media (min-width: 650px) {
+    max-width: 1200px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 1000px) {
+    max-width: 1300px;
+
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 1305px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
+export default CharacterListStyled;
