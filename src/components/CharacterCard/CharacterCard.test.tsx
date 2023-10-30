24a6b548c3454750f4723b6c9489259318f1908b
RubenRaMar <rubenfr1@hotmail.com>
2023-10-30 19:38:50 +0100

    Test CharacterCard component and their uses case

---
+++ src/components/CharacterCard/CharacterCard.test.tsx
@@ -0,0 +1,54 @@
+import { screen } from "@testing-library/react";
+import { characterMock } from "../../mocks/characters/characters";
+import { renderWithProviders } from "../../testUtils/renderWithProviders";
+import CharacterCard from "./CharacterCard";
+
+describe("Given a CharacterCard component", () => {
+  describe("When its invoked", () => {
+    const {
+      name,
+      location: { name: locationName },
+      gender,
+      status,
+    } = characterMock;
+
+    test("Then it should show four images that represent character data", () => {
+      const expectedAltTexts = [
+        `The status of the character, ${status}`,
+        `Of the character ${name}`,
+        `The gender of the personage, ${gender}`,
+        "The location of the character",
+      ];
+
+      renderWithProviders({
+        ui: <CharacterCard character={characterMock} position={0} />,
+      });
+
+      expectedAltTexts.forEach((expectedAltText) => {
+        const image = screen.getByAltText(expectedAltText);
+
+        expect(image).toBeInTheDocument();
+      });
+    });
+
+    test(`And a heading with the text ${name}`, () => {
+      renderWithProviders({
+        ui: <CharacterCard character={characterMock} position={0} />,
+      });
+
+      const heading = screen.getByRole("heading", { name: name });
+
+      expect(heading).toBeInTheDocument();
+    });
+
+    test(`And the text ${locationName}`, () => {
+      renderWithProviders({
+        ui: <CharacterCard character={characterMock} position={0} />,
+      });
+
+      const text = screen.getByText(locationName);
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});
--- src/mocks/apiTest/handlers.ts
+++ src/mocks/apiTest/handlers.ts
@@ -1,15 +1,16 @@
 import { http, HttpResponse } from "msw";
 import { apiPaths } from "../../constants/paths/paths";
 import { newCharactersStateMock } from "../characters/characters";
+import { CharacterStateStructure } from "../../types";
 
 export const handlers = [
   http.get(apiPaths.character, () => {
-    return HttpResponse.json(newCharactersStateMock, { status: 200 });
+    return HttpResponse.json<CharacterStateStructure>(newCharactersStateMock);
   }),
 ];
 
 export const errorHandlers = [
   http.get(apiPaths.character, () => {
-    return HttpResponse.json(null, { status: 401 });
+    return HttpResponse.json(undefined);
   }),
 ];
