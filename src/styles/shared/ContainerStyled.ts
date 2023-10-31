c2933afb0fef8e24cd39b3a39b3460fea0981c8a
RubenRaMar <rubenfr1@hotmail.com>
2023-10-31 19:59:52 +0100

    Implement styles to the Button component

--- src/styles/shared/ContainerStyled.ts
+++ src/styles/shared/ContainerStyled.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 const ContainerStyled = styled.div`
-  max-width: 800px;
   margin: 0 auto;
   padding-inline: 30px;
 `;
--- src/styles/shared/PageStyled.ts
+++ src/styles/shared/PageStyled.ts
@@ -9,9 +9,7 @@ const PageStyled = styled.main`
     color: ${themeColors.tertiary};
     background-color: ${themeColors.darkTransparent};
     font-size: ${themeFontsSize.large};
-    border: 1px solid ${themeColors.tertiary};
     border-radius: 15px;
-    box-shadow: 0px 0px 6px 1px ${themeColors.secondaryTransparent};
   }
 `;
 
