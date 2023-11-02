94f450a864c6588e50b3d310429a31a5f0ea25f3
RubenRaMar <rubenfr1@hotmail.com>
2023-11-02 23:38:35 +0100

    Test showMoreCharacters CharactersPage page and their uses case

--- src/pages/CharactersPage/CharactersPage.test.tsx
+++ src/pages/CharactersPage/CharactersPage.test.tsx
@@ -1,6 +1,8 @@
 import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../testUtils/renderWithProviders";
 import CharactersPage from "./CharactersPage";
+import userEvent from "@testing-library/user-event";
+import { store } from "../../store";
 
 describe("Given a CharactersPage page", () => {
   describe("When its rendered", () => {
@@ -14,6 +16,25 @@ describe("Given a CharactersPage page", () => {
       });
 
       expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and the user clicks on the 'next' button", () => {
+    test("Then it should call the function nextPage", async () => {
+      const expectedButtonText = "Show More";
+      const expectedNewCharacterLength = 8;
+
+      renderWithProviders({
+        ui: <CharactersPage />,
+      });
+
+      const button = screen.getByRole("button", { name: expectedButtonText });
+
+      await userEvent.click(button);
+
+      const charactersLength = store.getState().character.results.length;
+
+      expect(charactersLength).toBe(expectedNewCharacterLength);
     });
   });
 });
