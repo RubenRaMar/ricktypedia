e652e8cbc84c681e57d7418e00096179c22e64cd
RubenRaMar <rubenfr1@hotmail.com>
2023-11-04 14:44:35 +0100

    Implement loadCharacters to the CharactersPage

--- src/pages/CharactersPage/CharactersPage.tsx
+++ src/pages/CharactersPage/CharactersPage.tsx
@@ -1,16 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import CharactersPageStyled from "./CharactersPageStyled";
 import { useAppDispatch, useAppSelector } from "../../store";
-import {
-  loadCharactersActionCreator,
-  showMoreCharactersActionCreator,
-} from "../../store/characters/charactersSlice";
+import { showMoreCharactersActionCreator } from "../../store/characters/charactersSlice";
 import useCharacter from "../../hooks/useCharacter/useCharacter";
 import CharacterList from "../../components/CharacterList/CharacterList";
 import FormSearch from "../../components/FormSearch/FormSearch";
 import Button from "../../components/Button/Button";
+import _debounce from "debounce";
+import { apiPaths } from "../../constants/paths/paths";
 
 const CharactersPage = (): React.ReactElement => {
+  const isFirstRender = useRef(true);
   const charactersData = useAppSelector((store) => store);
   const {
     character: {
@@ -27,26 +27,13 @@ const CharactersPage = (): React.ReactElement => {
     }) => next
   );
 
-  const { getCharacterList } = useCharacter();
+  const { getCharacterList, loadCharacters } = useCharacter();
 
   useEffect(() => {
     (async () => {
-      const characters = await getCharacterList();
-
-      if (characters) {
-        dispatch(loadCharactersActionCreator(characters));
-
-        const preloadLink = document.createElement("link");
-        preloadLink.rel = "preload";
-        preloadLink.as = "image";
-        preloadLink.href = characters.results[0].image;
-
-        const head = document.head;
-        const firstChild = head.firstChild;
-        head.insertBefore(preloadLink, firstChild);
-      }
+      loadCharacters(apiPaths.character);
     })();
-  }, [dispatch, getCharacterList]);
+  }, [loadCharacters]);
 
   const handleClickToAction = async () => {
     if (nextPage) {
@@ -58,10 +45,23 @@ const CharactersPage = (): React.ReactElement => {
     }
   };
 
+  const handleSearchCharacters = useMemo(
+    () =>
+      _debounce(async (query: string) => {
+        if (isFirstRender.current) {
+          isFirstRender.current = false;
+          return;
+        }
+
+        loadCharacters(`${apiPaths.character}/?name=${query}`);
+      }, 500),
+    [loadCharacters]
+  );
+
   return (
     <CharactersPageStyled>
       <h1>Characters</h1>
-      <FormSearch />
+      <FormSearch onSearchChange={handleSearchCharacters} />
       {results.length <= 0 && (
         <span className="search-feedback">
           No characters matching your search were found
