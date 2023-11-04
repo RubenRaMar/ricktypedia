57b4dc6bc9f281ea75e2f26ba4dd072db825dbf5
RubenRaMar <rubenfr1@hotmail.com>
2023-11-04 14:42:42 +0100

    Add loadCharacters custom hook

--- src/hooks/useCharacter/useCharacter.ts
+++ src/hooks/useCharacter/useCharacter.ts
@@ -1,20 +1,19 @@
 import { useCallback } from "react";
 import axios from "axios";
 import { CharacterStateStructure } from "../../types";
-import { apiPaths } from "../../constants/paths/paths";
 import { useAppDispatch } from "../../store";
 import {
   hideLoadingActionCreator,
   showLoadingActionCreator,
 } from "../../store/ui/uiSlice";
+import { loadCharactersActionCreator } from "../../store/characters/charactersSlice";
+import { initialCharactersState } from "../../data/characters/characters";
 
 const useCharacter = () => {
   const dispatch = useAppDispatch();
 
   const getCharacterList = useCallback(
-    async (
-      path = apiPaths.character
-    ): Promise<CharacterStateStructure | undefined> => {
+    async (path: string): Promise<CharacterStateStructure | undefined> => {
       try {
         dispatch(showLoadingActionCreator());
 
@@ -32,7 +31,31 @@ const useCharacter = () => {
     [dispatch]
   );
 
-  return { getCharacterList };
+  const loadCharacters = useCallback(
+    async (path: string) => {
+      const characters = await getCharacterList(path);
+
+      if (characters) {
+        dispatch(loadCharactersActionCreator(characters));
+
+        const preloadLink = document.createElement("link");
+        preloadLink.rel = "preload";
+        preloadLink.as = "image";
+        preloadLink.href = characters.results[0].image;
+
+        const head = document.head;
+        const firstChild = head.firstChild;
+        head.insertBefore(preloadLink, firstChild);
+      }
+
+      if (path.includes("?name=") && !characters) {
+        dispatch(loadCharactersActionCreator(initialCharactersState));
+      }
+    },
+    [dispatch, getCharacterList]
+  );
+
+  return { getCharacterList, loadCharacters };
 };
 
 export default useCharacter;
