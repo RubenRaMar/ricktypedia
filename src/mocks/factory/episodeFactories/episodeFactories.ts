ed5106af8771e679306b86cb523efafe6aacbe25
RubenRaMar <rubenfr1@hotmail.com>
2023-11-12 01:32:31 +0100

    Refactor episodes mocks using factories

--- src/mocks/factory/episodeFactories/episodeFactories.ts
+++ src/mocks/factory/episodeFactories/episodeFactories.ts
@@ -1,8 +1,11 @@
 import { Factory } from "fishery";
 import { faker } from "@faker-js/faker";
-import { EpisodeDataStateStructure } from "../../../data/episodes/types";
+import {
+  EpisodeDataApiStructure,
+  EpisodeDataStateStructure,
+} from "../../../data/episodes/types";
 
-const episodeFactory = Factory.define<EpisodeDataStateStructure>(() => ({
+const episodeStateFactory = Factory.define<EpisodeDataStateStructure>(() => ({
   id: faker.number.int(),
   name: faker.person.firstName(),
   episode: faker.string.alpha({ length: 10 }),
@@ -18,10 +21,34 @@ const episodeFactory = Factory.define<EpisodeDataStateStructure>(() => ({
   url: faker.string.alpha({ length: 10 }),
 }));
 
-export const episodeMockFactory = (data?: EpisodeDataStateStructure) =>
-  episodeFactory.build(data);
+const episodeApiFactory = Factory.define<EpisodeDataApiStructure>(() => ({
+  id: faker.number.int(),
+  name: faker.person.firstName(),
+  episode: faker.string.alpha({ length: 10 }),
+  characters: [
+    faker.internet.url({ appendSlash: true }),
+    faker.internet.url({ appendSlash: true }),
+    faker.internet.url({ appendSlash: true }),
+    faker.internet.url({ appendSlash: true }),
+    faker.internet.url({ appendSlash: true }),
+  ],
+  air_date: faker.string.alpha({ length: 10 }),
+  created: faker.string.alpha({ length: 10 }),
+  url: faker.string.alpha({ length: 10 }),
+}));
 
-export const episodeMocksFactory = (
+export const episodeStateMockFactory = (data?: EpisodeDataStateStructure) =>
+  episodeStateFactory.build(data);
+
+export const episodesStateMocksFactory = (
   totalMocks: number,
   data?: EpisodeDataStateStructure
-) => episodeFactory.buildList(totalMocks, data);
+) => episodeStateFactory.buildList(totalMocks, data);
+
+export const episodeApiMockFactory = (data?: EpisodeDataApiStructure) =>
+  episodeApiFactory.build(data);
+
+export const episodesApiMocksFactory = (
+  totalMocks: number,
+  data?: EpisodeDataApiStructure
+) => episodeApiFactory.buildList(totalMocks, data);
