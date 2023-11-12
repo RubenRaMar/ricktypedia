b4dc152847575ca710c6b306481c3b93bf012cd9
RubenRaMar <rubenfr1@hotmail.com>
2023-11-12 23:43:17 +0100

    Test CharactersPage page and their uses case

--- src/pages/CharactersPage/CharactersPage.test.tsx
+++ src/pages/CharactersPage/CharactersPage.test.tsx
@@ -3,10 +3,18 @@ import { renderWithProviders } from "../../testUtils/renderWithProviders";
 import CharactersPage from "./CharactersPage";
 import userEvent from "@testing-library/user-event";
 import { store } from "../../store";
+import {
+  currentCharactersStateMock,
+  initialCharactersStateMock,
+} from "../../mocks/charactersMocks/charactersMocks";
+import { server } from "../../mocks/apiTest/node";
+import { errorHandlers } from "../../mocks/apiTest/handlers";
 
 describe("Given a CharactersPage page", () => {
+  const expectedButtonText = "Show More";
+
   describe("When its rendered", () => {
-    test("Then it should show a heading with de text 'Characters'", async () => {
+    test("Then it should show a heading with the text 'Characters'", async () => {
       const expectedHeadingText = /characters/i;
 
       renderWithProviders({ ui: <CharactersPage /> });
@@ -17,22 +25,75 @@ describe("Given a CharactersPage page", () => {
 
       expect(heading).toBeInTheDocument();
     });
+
+    test(`And the heading with ${currentCharactersStateMock.results[0].name}'s name`, async () => {
+      const expectedHeadingText = currentCharactersStateMock.results[0].name;
+
+      renderWithProviders({
+        ui: <CharactersPage />,
+        preloadedState: { character: currentCharactersStateMock },
+      });
+
+      const heading = screen.getByRole("heading", {
+        name: expectedHeadingText,
+      });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe("When its rendered but there aren't characters to show", () => {
+    test("Then it should show the text 'No characters matching your search were found'", async () => {
+      server.resetHandlers(...errorHandlers);
+
+      const text = "No characters matching your search were found";
+
+      renderWithProviders({
+        ui: <CharactersPage />,
+        preloadedState: { character: initialCharactersStateMock },
+      });
+
+      const textElement = screen.getByText(text);
+
+      expect(textElement).toBeInTheDocument();
+    });
+
+    test("And the 'Show More' button is disabled", () => {
+      renderWithProviders({
+        ui: <CharactersPage />,
+        preloadedState: {
+          character: {
+            ...currentCharactersStateMock,
+            info: { ...currentCharactersStateMock.info, next: null },
+          },
+        },
+      });
+
+      const showMoreButton = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+
+      expect(showMoreButton).toBeDisabled();
+    });
   });
 
   describe("When it is rendered and the user clicks on the 'next' button", () => {
     test("Then it should call the function nextPage", async () => {
-      const expectedButtonText = "Show More";
       const expectedNewCharacterLength = 8;
 
       renderWithProviders({
         ui: <CharactersPage />,
       });
 
-      const button = screen.getByRole("button", { name: expectedButtonText });
+      const showMoreButton = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
 
-      await userEvent.click(button);
+      await userEvent.click(showMoreButton);
 
       const charactersLength = store.getState().character.results.length;
+
+      await userEvent.click(showMoreButton);
 
       expect(charactersLength).toBe(expectedNewCharacterLength);
     });
