6424a1d66c9a2c901f0dc482da221b59a34e8b86
RubenRaMar <rubenfr1@hotmail.com>
2023-11-18 22:28:58 +0100

    Test EpisodesPage page and their uses case

--- src/pages/EpisodesPage/EpisodesPage.test.tsx
+++ src/pages/EpisodesPage/EpisodesPage.test.tsx
@@ -1,11 +1,17 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import { renderWithProviders } from "../../testUtils/renderWithProviders";
 import EpisodesPage from "./EpisodesPage";
+import {
+  currentEpisodeStateMock,
+  initialEpisodeStateMock,
+} from "../../mocks/episodesMocks/episodesMocks";
 import { server } from "../../mocks/apiTest/node";
 import { errorHandlers } from "../../mocks/apiTest/handlers";
 
 describe("Given a EpisodesPage page", () => {
   describe("When its rendered", () => {
+    const expetedEpisodeName = currentEpisodeStateMock.episodes[0].name;
+
     test("Then it should show the 'Episodes' text in the heading", () => {
       const episodeText = /episodes/i;
 
@@ -16,16 +22,34 @@ describe("Given a EpisodesPage page", () => {
       expect(heading).toBeInTheDocument();
     });
 
-    test("Then it should show the 'Episodes' text in the heading", () => {
-      server.resetHandlers(...errorHandlers);
+    test(`And a heading with a ${expetedEpisodeName}'s episode name`, async () => {
+      renderWithProviders({
+        ui: <EpisodesPage />,
+        preloadedState: { episode: currentEpisodeStateMock },
+      });
 
-      const episodeText = /episodes/i;
-
-      renderWithProviders({ ui: <EpisodesPage /> });
-
-      const heading = screen.getByRole("heading", { name: episodeText });
+      const heading = screen.getByRole("heading", {
+        name: expetedEpisodeName,
+      });
 
       expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe("And there aren't episodes to show", () => {
+    test("Then it should show the text 'No episodes matching your search were found'", async () => {
+      server.resetHandlers(...errorHandlers);
+
+      const text = "No episodes matching your search were found";
+
+      renderWithProviders({
+        ui: <EpisodesPage />,
+        preloadedState: { episode: initialEpisodeStateMock },
+      });
+
+      const textElement = await screen.findByText(text);
+
+      await waitFor(() => expect(textElement).toBeInTheDocument());
     });
   });
 });
