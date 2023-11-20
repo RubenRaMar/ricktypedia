d9b7fd45e90154322e32ad7f2d18d57c64cdcaf7
RubenRaMar <rubenfr1@hotmail.com>
2023-11-20 21:26:14 +0100

    Test EpisodesPage page and their uses case

--- src/mocks/factory/infoFactories/infoFactories.ts
+++ src/mocks/factory/infoFactories/infoFactories.ts
@@ -1,11 +1,12 @@
 import { Factory } from "fishery";
 import { InfoStructure } from "../../../data/info/types";
 import { faker } from "@faker-js/faker";
+import { apiPaths } from "../../../constants/paths/paths";
 
 const infoFactory = Factory.define<InfoStructure>(() => ({
   count: faker.number.int(),
   pages: faker.number.int(),
-  next: faker.string.alpha({ length: 10 }),
+  next: apiPaths.episode,
   prev: faker.string.alpha({ length: 10 }),
 }));
 
