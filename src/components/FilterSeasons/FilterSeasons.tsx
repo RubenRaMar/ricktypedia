77488993441cf95992b5025adf93909944e109e2
RubenRaMar <rubenfr1@hotmail.com>
2023-12-03 21:59:08 +0100

    Add FilterSeasons component

---
+++ src/components/FilterSeasons/FilterSeasons.tsx
@@ -0,0 +1,33 @@
+import FilterSeasonsStyled from "./FilterSeasonsStyled";
+
+interface FilterSeasonsProps {
+  onFilterChange: (season: string) => void;
+}
+
+const FilterSeasons = ({
+  onFilterChange,
+}: FilterSeasonsProps): React.ReactElement => {
+  const handleSeasonFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const season = event.target.value;
+
+    if (onFilterChange) {
+      onFilterChange(season);
+    }
+  };
+
+  return (
+    <FilterSeasonsStyled>
+      <select onChange={handleSeasonFilter}>
+        <option hidden>Seasons</option>
+        <option value="">All seasons</option>
+        <option value="S01">Season 1</option>
+        <option value="S02">Season 2</option>
+        <option value="S03">Season 3</option>
+        <option value="S04">Season 4</option>
+        <option value="S05">Season 5</option>
+      </select>
+    </FilterSeasonsStyled>
+  );
+};
+
+export default FilterSeasons;
