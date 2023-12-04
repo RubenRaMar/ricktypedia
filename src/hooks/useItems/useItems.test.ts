9edd1c92ac8d9b69d95054ca6efe848820e59eae
RubenRaMar <rubenfr1@hotmail.com>
2023-12-04 01:24:06 +0100

    Test searchItems function and their uses case

--- src/hooks/useItems/useItems.test.ts
+++ src/hooks/useItems/useItems.test.ts
@@ -3,6 +3,10 @@ import useItems from "./useItems";
 import { wrapWithProviders } from "../../testUtils/renderWithProviders";
 import { HandleItemsRealTimeSearch } from "./types";
 
+beforeEach(() => {
+  vi.clearAllMocks();
+});
+
 describe("Given a handleItemsRealTimeSearch function", () => {
   describe("When its invoked with an 'exemple' query, a mockLoadItems function and a url 'https://example'", () => {
     test("Then it should call the mockLoadItems function with a url 'https://example/?name=example'", async () => {
@@ -17,15 +21,57 @@ describe("Given a handleItemsRealTimeSearch function", () => {
 
       const {
         result: {
-          current: { handleItemsRealTimeSearch: handleSearchItems },
+          current: { handleItemsRealTimeSearch },
         },
       } = renderHook(() => useItems(), { wrapper: wrapWithProviders });
 
-      handleSearchItems(itemsRealTimeSearch);
+      handleItemsRealTimeSearch(itemsRealTimeSearch);
 
       await waitFor(() => {
         expect(mockLoadItems).toHaveBeenCalledWith(expectedUrl);
       });
+    });
+  });
+});
+
+describe("Given a searchItems function", () => {
+  const actualSearch = { current: "Morty" };
+  const nameToSearch = "Rick";
+  const onSearchChange = vi.fn();
+
+  describe("When its rendered and receives a onSearchChange function, a 'Morty' actual search and 'Rick' name to search", () => {
+    test("Then it should call the onSearchChange function", () => {
+      const {
+        result: {
+          current: { searchItems },
+        },
+      } = renderHook(() => useItems(), { wrapper: wrapWithProviders });
+
+      searchItems({
+        actualSearch,
+        nameToSearch,
+        onSearchChange,
+      });
+
+      expect(onSearchChange).toHaveBeenCalled();
+    });
+  });
+
+  describe("When its rendered and receives a onSearchChange function, a 'Morty' actual search and 'Morty' name to search", () => {
+    test("Then it should call the onSearchChange function", () => {
+      const {
+        result: {
+          current: { searchItems },
+        },
+      } = renderHook(() => useItems(), { wrapper: wrapWithProviders });
+
+      searchItems({
+        actualSearch,
+        nameToSearch,
+        onSearchChange,
+      });
+
+      expect(onSearchChange).not.toHaveBeenCalled();
     });
   });
 });
