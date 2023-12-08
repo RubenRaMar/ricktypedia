f6c77495e56f0059405ba90434655711751f21a5
RubenRaMar <rubenfr1@hotmail.com>
2023-12-08 23:04:51 +0100

    Test FormFilter component and their uses case

--- src/components/FormFilter/FormFilter.test.tsx
+++ src/components/FormFilter/FormFilter.test.tsx
@@ -4,38 +4,19 @@ import userEvent from "@testing-library/user-event";
 import { store } from "../../store";
 import { server } from "../../mocks/apiTest/node";
 import { errorHandlers } from "../../mocks/apiTest/handlers";
-import FormSearch from "./FormSearch";
+import FormFilter from "./FormFilter";
 
-afterEach(() => {
-  vi.clearAllMocks();
-});
-
-describe("Given a FormSearch component", () => {
+describe("Given a FormFilter component", () => {
   const actionOnClick = vi.fn();
   const searchTextInput = "search";
   const searchTextButton = "search-button";
   const expectText = "morty";
 
   describe("When its rendered", () => {
-    test("Then it should show a 'search' imput", () => {
-      renderWithProviders({
-        ui: (
-          <FormSearch
-            placeholder="Pilot, Lawnmower Dog..."
-            onSearchChange={actionOnClick}
-          />
-        ),
-      });
-
-      const searchImput = screen.getByLabelText(searchTextInput);
-
-      expect(searchImput).toBeInTheDocument();
-    });
-
     test("Then it should show a 'search-button' button", () => {
       renderWithProviders({
         ui: (
-          <FormSearch
+          <FormFilter
             placeholder="Pilot, Lawnmower Dog..."
             onSearchChange={actionOnClick}
           />
@@ -50,49 +31,6 @@ describe("Given a FormSearch component", () => {
     });
   });
 
-  describe("When rendering and a user types 'Morty' in search input", () => {
-    test("Then it should show 'morty' in the search input", async () => {
-      renderWithProviders({
-        ui: (
-          <FormSearch
-            placeholder="Pilot, Lawnmower Dog..."
-            onSearchChange={actionOnClick}
-          />
-        ),
-      });
-
-      const searchImput = screen.getByLabelText(searchTextInput);
-
-      await userEvent.type(searchImput, expectText);
-
-      expect(searchImput).toHaveValue(expectText);
-    });
-  });
-
-  describe("And if user click on the search button", () => {
-    test("Then it should call the actionOnClick function", async () => {
-      renderWithProviders({
-        ui: (
-          <FormSearch
-            placeholder="Pilot, Lawnmower Dog..."
-            onSearchChange={actionOnClick}
-          />
-        ),
-      });
-
-      const searchImput = screen.getByLabelText(searchTextInput);
-      const searchButton = screen.getByRole("button", {
-        name: searchTextButton,
-      });
-
-      await userEvent.type(searchImput, expectText);
-
-      await userEvent.click(searchButton);
-
-      expect(actionOnClick).toHaveBeenCalled();
-    });
-  });
-
   describe("And if you click on the search button and there aren't characters", () => {
     test("Then it should be a list without characters", async () => {
       server.resetHandlers(...errorHandlers);
@@ -101,7 +39,7 @@ describe("Given a FormSearch component", () => {
 
       renderWithProviders({
         ui: (
-          <FormSearch
+          <FormFilter
             placeholder="Ricky, Morty, Summer..."
             onSearchChange={actionOnClick}
           />
