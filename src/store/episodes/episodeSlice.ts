824ad3b859de940e519950fad52b61f9532904cf
RubenRaMar <rubenfr1@hotmail.com>
2023-12-10 02:17:06 +0100

    Add loadEpisodeById mini reduced to the episodesSlice slice

--- src/store/episodes/episodeSlice.ts
+++ src/store/episodes/episodeSlice.ts
@@ -1,6 +1,10 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { initialEpisodesState } from "../../data/episodes/episodes";
-import { NewEpisodesStateStructure } from "../../data/episodes/types";
+import {
+  EpisodeDataStructure,
+  EpisodesStateStructure,
+  NewEpisodesStateStructure,
+} from "../../data/episodes/types";
 
 const episodeSlice = createSlice({
   initialState: initialEpisodesState,
@@ -9,20 +13,30 @@ const episodeSlice = createSlice({
     loadEpisodes: (
       currentEpisodeState,
       action: PayloadAction<NewEpisodesStateStructure>
-    ) => ({
+    ): EpisodesStateStructure => ({
       ...currentEpisodeState,
       ...action.payload,
     }),
-    clearEpisodes: () => ({
+    clearEpisodes: (): EpisodesStateStructure => ({
       ...initialEpisodesState,
     }),
     showMoreEpisodes: (
       currentEpisodeState,
       action: PayloadAction<NewEpisodesStateStructure>
-    ) => ({
+    ): EpisodesStateStructure => ({
       ...currentEpisodeState,
       episodes: [...currentEpisodeState.episodes, ...action.payload.episodes],
       info: action.payload.info,
+    }),
+    loadEpisodeById: (
+      currentEpisodeState,
+      action: PayloadAction<EpisodeDataStructure>
+    ): EpisodesStateStructure => ({
+      ...currentEpisodeState,
+      episodeData: {
+        ...currentEpisodeState.episodeData,
+        ...action.payload,
+      },
     }),
   },
 });
@@ -31,6 +45,7 @@ export const {
   loadEpisodes: loadEpisodesActionCreator,
   showMoreEpisodes: showMoreEpisodesActionCreator,
   clearEpisodes: clearEpisodesActionCreator,
+  loadEpisodeById: loadEpisodeByIdActionCreator,
 } = episodeSlice.actions;
 
 export const episodeReducer = episodeSlice.reducer;
--- src/store/ui/uiSlice.ts
+++ src/store/ui/uiSlice.ts
@@ -1,16 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { initialUiState } from "../../data/ui/ui";
+import { UiStructure } from "../../data/ui/types";
 
 const uiSlice = createSlice({
   name: "ui",
   initialState: initialUiState,
   reducers: {
-    showLoading: (currentUiState) => ({
+    showLoading: (currentUiState): UiStructure => ({
       ...currentUiState,
       isLoading: true,
     }),
 
-    hideLoading: (currentUiState) => ({
+    hideLoading: (currentUiState): UiStructure => ({
       ...currentUiState,
       isLoading: false,
     }),
